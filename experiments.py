ece8cee918cfe001e359a80ef7e3f49f029899a9
Pradeep-Vanapalli <pradeep.vanapalli121@gmail.com>
2024-01-19 20:21:56 +0530

    adding files

---
+++ experiments.py
@@ -0,0 +1,23 @@
+import mlflow
+
+def calculator(a, b, operation=None):
+    if operation=="add":
+        return (a+b)
+    if operation=="sub":
+        return (a-b)
+    if operation=="mul":
+        return (a*b)
+    if operation=="div":
+        return (a/b)
+
+if __name__=="__main__":
+    a,b,opt = 10, 2, "mul"
+    with mlflow.start_run():
+        result=calculator(a, b, opt)
+        mlflow.log_param("a", a)
+        mlflow.log_param("b", b)
+        mlflow.log_param("opt", opt)
+        print(f"my result regarding {opt} is {result}")
+        mlflow.log_param("result", result)
+
+    
